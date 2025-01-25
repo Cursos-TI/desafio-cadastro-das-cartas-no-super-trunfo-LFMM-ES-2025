e2d78dcc1a6a84934226aeaca0fa8bbc05aa7f62
LFMM <luiz.fernando.m.marques@gmail.com>
2025-01-25 14:45:54 +0000

    versao 2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,13 +3,19 @@
  Nível Novato - Cadastro das Cartas
  Aluno: Luiz Fernando Machado Marques
  Matrícula: 202501463101
+ 
+ Devido a uma limitação do scanf, não e possível o uso de espaço nos nomes das cidades.
+
+ Super Trunfo com o tema "Países". As cartas serão divididas por estados, cada um com quatro cidades.
+ Cada país será dividido em oito estados, identificados pelas letras de A a H. Cada estado terá quatro cidades,
+ numeradas de 1 a 4. A combinação da letra do estado e o número da cidade define o código da carta (por exemplo, A01, A02, B01, B02).
 */
 
 #include <stdio.h>
 
 int main(){
-    char codigo_carta[] = "";
-    char nome_cidade[] = "";
+    char codigo_carta[4]; // 3 CARACTERES MAIS O ZERO QUE INDICA O FIM DA STRING
+    char nome_cidade[50]; // 49 CARACTERES MAIS O ZERO QUE INDICA O FIM DA STRING
     int populacao = 0;
     float area = 0;
     float pib = 0;
@@ -24,10 +30,10 @@ int main(){
     printf("Cadastro de Cartas\n\n");
 
 	printf("Digite o código da carta:\n");
-	scanf("%s", &codigo_carta);
+	scanf("%s", codigo_carta);
 
-	printf("Digite o nome da cidade:\n");
-	scanf("%s", &nome_cidade);
+	printf("Digite o nome da cidade (não use espaço, use \'-\' ou \'_\'):\n");
+	scanf("%s", nome_cidade);
 
 	printf("Digite a populaçâo da cidade:\n");
 	scanf("%d", &populacao);
